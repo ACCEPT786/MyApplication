a7aa31f041cd42577b8718e5e4a4d8b819fb69d3
Frank <444709834@qq.com>
2019-01-31 10:34:55 +0800

    commit

--- app/src/main/java/com/example/myapplication/Fragment2.java
+++ app/src/main/java/com/example/myapplication/Fragment2.java
@@ -1,39 +1,13 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
 
 
 
 public class Fragment2 extends Fragment {
-    private TextView textView;
-    private Button button2;
-    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view=inflater.inflate(R.layout.fragment2,container,false);
-        return view;
-    }
-
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        textView=getActivity().findViewById(R.id.textView2);
-        button2=getActivity().findViewById(R.id.button2);
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //Toast.makeText(getActivity(),"Fragment2",Toast.LENGTH_SHORT).show();
-            }
-        });
-
-
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
     }
 }
